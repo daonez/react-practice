21bc7a7531578f3d95b8a8a36cdbfeead51271c9
unknown <cyongchoi@gmail.com>
2020-03-10 00:59:02 +0900

    practice of child, prop

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React ,{Component} from "react";
 import "./App.css";
+import Person from './Person/Person'
 
 class App extends React.Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi, I am a react app</h1>
-    //   </div>
-    // );
+    return (
+      <div className="App">
+        <h1>Hi, I am a react app</h1>
+        <Person name='Fran' age="28"/>
+        <Person name="Manu" age="30">My Hobbies: Racing</Person> 
+        <Person/>
+
+      </div>
+    );
     // return React.createElement('div',null,'h1','Hi, I am a react app')
-    return React.createElement('div',{className:"App"},React.createElement('h1',null,'Does this work now'))
+   // return React.createElement('div',{className:"App"},React.createElement('h1',null,'Does this work now'))
   }
 }
 
